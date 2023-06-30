201b19b6dbbbcf1f152f90b9cbd532ab37535064
Ben Myers <benl.myers01@gmail.com>
2023-06-30 01:03:10 -0700

    ⭐️ Update example

--- Examples/MagneticScrollExample/MagneticScrollExample/ContentView.swift
+++ Examples/MagneticScrollExample/MagneticScrollExample/ContentView.swift
@@ -11,13 +11,31 @@ import MagneticScroll
 struct ContentView: View {
   var body: some View {
     VStack {
-      MagneticScrollView {
-        Block {
-          Text("Auto-fit height view")
-        }
-        Block(height: 100.0) {
-          Text("Set height view")
-        }
+      MagneticScrollView(spacing: 12.0) {
+        Text("Hello World")
+          .frame(maxWidth: .infinity)
+          .frame(height: 100.0)
+          .background(Color.red)
+        Text("My name is Ben")
+          .frame(maxWidth: .infinity)
+          .frame(height: 200.0)
+          .background(Color.yellow)
+        Text("Hello World")
+          .frame(maxWidth: .infinity)
+          .frame(height: 300.0)
+          .background(Color.red)
+        Text("My name is Ben")
+          .frame(maxWidth: .infinity)
+          .frame(height: 400.0)
+          .background(Color.yellow)
+        Text("Hello World")
+          .frame(maxWidth: .infinity)
+          .frame(height: 300.0)
+          .background(Color.red)
+        Text("My name is Ben")
+          .frame(maxWidth: .infinity)
+          .frame(height: 400.0)
+          .background(Color.yellow)
       }
     }
   }
