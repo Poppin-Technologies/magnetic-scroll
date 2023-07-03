1706a68cb81a0854de39c5695a6469d51f0078f6
grandsir <69051988+grandsir@users.noreply.github.com>
2023-07-03 23:26:41 +0300

    🧾 Create MagneticScrollConfiguration

---
+++ Sources/MagneticScroll/Logic/MagneticScrollConfiguration.swift
@@ -0,0 +1,42 @@
+//
+//  MagneticScrollConfiguration.swift
+//  
+//
+//  Created by Demirhan Mehmet Atabey on 3.07.2023.
+//
+
+import Foundation
+
+/**
+ Configuration for `MagneticScrollView`
+ */
+internal class MagneticScrollConfiguration {
+  /// If the `activeBlock` should be changed on tap gesture
+  var changesActiveBlockOnTapGesture: Bool = true
+  /// Value that decides how `MagneticScrolLView` should react to `Velocity` of `ScrollView`
+  var scrollVelocityThreshold: Double = 0.5
+  ///
+  var triggersHapticFeedbackOnBlockChange = true
+  ///
+  var scrollAnimationDuration: Double = 0.25
+  
+  
+  // singleton
+  static let shared = MagneticScrollConfiguration()
+  
+  private init() {
+    
+  }
+}
+
+
+extension MagneticScrollConfiguration : Equatable {
+  static func == (lhs: MagneticScrollConfiguration, rhs: MagneticScrollConfiguration) -> Bool {
+    return (
+      lhs.changesActiveBlockOnTapGesture == rhs.changesActiveBlockOnTapGesture &&
+      lhs.scrollVelocityThreshold == rhs.scrollVelocityThreshold &&
+      lhs.triggersHapticFeedbackOnBlockChange == rhs.triggersHapticFeedbackOnBlockChange &&
+      lhs.scrollAnimationDuration == rhs.scrollAnimationDuration
+    )
+  }
+}
