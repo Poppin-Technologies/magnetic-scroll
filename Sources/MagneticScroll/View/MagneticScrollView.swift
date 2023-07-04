675fa2f282aa4c0d453603bc5e519f7bdedc59b7
grandsir <69051988+grandsir@users.noreply.github.com>
2023-07-04 12:45:47 +0300

    ⚒️ More Customization

--- Sources/MagneticScroll/View/MagneticScrollView.swift
+++ Sources/MagneticScroll/View/MagneticScrollView.swift
@@ -75,7 +75,12 @@ public struct MagneticScrollView<Content>: View where Content: View {
 
 public extension MagneticScrollView {
   /**
+   Sets whether the active block should be changed on tap gesture.
    
+   - Parameters:
+     - value: A Boolean value indicating whether the active block should be changed on tap gesture. Default value is `true`.
+   
+   - Returns: The `MagneticScrollView` instance with the updated configuration.
    */
   func changesActiveBlockOnTapGesture(_ value: Bool = true) -> MagneticScrollView {
     MagneticScrollConfiguration.shared.changesActiveBlockOnTapGesture = value
@@ -83,7 +88,14 @@ public extension MagneticScrollView {
   }
   
   /**
-    
+   Sets the velocity threshold for `MagneticScrollView` to react to scroll view velocity.
+   
+   - Parameters:
+     - threshold: A double value representing the scroll velocity threshold.
+                  Higher values result in faster scrolling to the calculated block,
+                  while lower values result in slower scrolling to the calculated block.
+   
+   - Returns: The `MagneticScrollView` instance with the updated configuration.
    */
   func velocityThreshold(_ threshold: Double) -> MagneticScrollView {
     MagneticScrollConfiguration.shared.scrollVelocityThreshold = threshold
@@ -91,20 +103,65 @@ public extension MagneticScrollView {
   }
   
   /**
-    
+   Sets whether haptic feedback should be triggered when the active block changes.
+   
+   - Parameters:
+     - bool: A Boolean value indicating whether haptic feedback should be triggered on block change. Default value is `true`.
+   
+   - Returns: The `MagneticScrollView` instance with the updated configuration.
    */
   func triggersHapticFeedbackOnBlockChange(_ bool: Bool = true) -> MagneticScrollView {
     MagneticScrollConfiguration.shared.triggersHapticFeedbackOnBlockChange = bool
     return self
   }
+  
+  
+  /**
+   Sets whether haptic feedback should be triggered when the active block changes.
+   
+   - Parameters:
+     - bool: A Boolean value indicating whether haptic feedback should be triggered on block change. Default value is `true`.
+   
+   - Returns: The `MagneticScrollView` instance with the updated configuration.
+   */
+  func triggersHapticFeedbackOnActiveBlockChange(_ bool: Bool = true) -> MagneticScrollView {
+    MagneticScrollConfiguration.shared.triggersHapticFeedbackOnActiveBlockChange = bool
+    return self
+  }
  
+  /**
+   Sets whether the form style should be enabled or not.
+
+   The `formStyle` configuration allows you to change the blocks by tapping when scrolling but not when not scrolling.
+   */
   func formStyle(_ bool: Bool = true) -> MagneticScrollView {
     MagneticScrollConfiguration.shared.formStyle = bool
     return self
   }
   
+  /**
+   Sets the scroll animation duration when changing the active block.
+   
+   - Parameters:
+     - duration: A double value representing the scroll animation duration in seconds.
+   
+   - Returns: The `MagneticScrollView` instance with the updated configuration.
+   */
   func scrollAnimationDuration(_ duration: Double) -> MagneticScrollView {
     MagneticScrollConfiguration.shared.scrollAnimationDuration = duration
+    return self
+  }
+  
+  /**
+   The timeout duration needed to change a block to another.
+   
+   - Parameters:
+     - duration: A double value representing the timeout duration in seconds.
+   
+   - Returns: The `MagneticScrollView` instance with the updated configuration.
+   */
+  func setTimeout(_ duration: Double) -> MagneticScrollView {
+    MagneticScrollConfiguration.shared.timeoutNeeded = duration
     return self
   }
 }
